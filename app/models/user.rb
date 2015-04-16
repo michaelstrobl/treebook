57f8354c2f274392c6d3fd199a9c322c6acf4ef4
michaelstrobl <test@test.com>
2015-04-16 13:12:28 +0200

    Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
     attr_accessible :email, :password, :password_confirmation, 
     	:remember_me, :first_name, :last_name, :profile_name
          
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :profile_name, presence: true,
+    						uniqueness:true,
+    						format: {
+    							with: /\A[a-zA-Z\-\_]+\Z/,
+    							message: "must be formatted correctly."
+    						}
 end
